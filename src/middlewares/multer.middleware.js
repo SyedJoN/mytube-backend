6b41032ad6e5b324f4891e229b1d4dc62fae39c4
Jon <muhammadjoncs16@gmail.com>
2025-03-06 02:44:47 +0500

    cloudinary and multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // use unique bhut sari same files ek hi naam ki ni hon later change it
+  },
+});
+
+export const upload = multer({
+  storage,
+});
